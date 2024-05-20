d0f2284a1f565e57396d7d7b101c083cee030f25
Johnson Kow <jkow95@gmail.com>
2024-05-20 13:54:06 -0400

    Using an S3 bucket to provide image links

--- components/Shop/ShoeCard.js
+++ components/Shop/ShoeCard.js
@@ -1,28 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import Text from "../DesignKit/Text";
+import Image from "next/image";
 
 const ShoeCard = ({ shoeData, setSelectedShoeData }) => {
-
+  const { title, price, imageUrl } = shoeData || {};
   return (
     <Wrapper onClick={() => setSelectedShoeData(shoeData)}>
-      <PlaceholderImage />
+      <ImageContainer>
+        {imageUrl ? (
+          <Image src={imageUrl} alt={title} layout="fill" objectFit="cover" />
+        ) : null}
+      </ImageContainer>
       <Text font="roboto" weight="semibold" color="dark-orange">
         Just In
       </Text>
-      <Text font="roboto">Nike Dunk low Retro</Text>
+      <Text font="roboto">{title}</Text>
       <Text font="roboto">Men's Shoes</Text>
       <Text font="roboto">4 Colors</Text>
-      <Text font="roboto">$115</Text>
+      <Text font="roboto">${price}</Text>
     </Wrapper>
   );
 };
 
 export default ShoeCard;
-const PlaceholderImage = styled.div`
+const ImageContainer = styled.div`
   width: 100%;
   height: 70%;
-  background: black;
+  position: relative;
 `;
 const Wrapper = styled.div`
   width: 100%;
